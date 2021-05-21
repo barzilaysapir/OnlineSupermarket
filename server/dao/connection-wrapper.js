4c81fcfb15ee7328da6daeb64f2b0d9cf3ad6b49
barzilaysapir <barzilaysapir@gmail.com>
2021-05-21 17:07:56 +0300

    Change back DB connection to localhost

--- server/dao/connection-wrapper.js
+++ server/dao/connection-wrapper.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-    host: "34.65.237.32",
+    host: "localhost",
     user: "root",
     password: "1234",
     database: "supermarket",
